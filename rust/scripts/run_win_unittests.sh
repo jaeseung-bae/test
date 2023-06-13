97a7cd44ceaa5aa49c12dbfd9dada811fb188217
jaeseung.bae <jaeseung.bae@linecorp.com>
2023-06-13 20:14:44 +0900

    fix test

--- rust/scripts/run_win_unittests.sh
+++ rust/scripts/run_win_unittests.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
-# Iterate over the executables and run them
-#for executable in $executables; do
-#    echo "Running $executable"
-#    output=$($executable 2>&1)
-#    if [ $? -ne 0 ]; then
-#      echo "::error file=$executable,line=1,col=0,endColumn=10::$output"
-#      exit 1
-#    fi
-#done
-#
-
-for file in *; do
-  if [ -x "$file" ]; then
-    ./"$file"
+for executable in *; do
+  if [ -x "$executable" ]; then
+    ./"$executable"
   fi
 done
 
