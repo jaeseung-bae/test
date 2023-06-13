22bb22a3875a2165e3231cc1d24df5f5eb18560d
jaeseung.bae <jaeseung.bae@linecorp.com>
2023-06-13 20:05:14 +0900

    fix test

--- rust/scripts/run_win_unittests.sh
+++ rust/scripts/run_win_unittests.sh
@@ -10,15 +10,9 @@
 #done
 #
 
-source $GITHUB_ENV
-
-echo "win unittests=${WIN_UNITTESTS}"
-for executable in $WIN_UNITTESTS; do
-    echo "Running $WIN_UNITTESTS"
-    output=$($executable 2>&1)
-    if [ $? -ne 0 ]; then
-      echo "::error file=$WIN_UNITTESTS,line=1,col=0,endColumn=10::$output"
-      exit 1
-    fi
+for file in *; do
+  if [ -x "$file" ]; then
+    ./"$file"
+  fi
 done
 
