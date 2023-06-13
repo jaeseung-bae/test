0e10ba67f3a8038806382f3f6f3c9594157641cf
jaeseung.bae <jaeseung.bae@linecorp.com>
2023-06-13 20:26:14 +0900

    fix test

--- rust/scripts/run_win_unittests.sh
+++ rust/scripts/run_win_unittests.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-for executable in *; do
-  if [ -x "$executable" ]; then
-    ./"$executable"
+
+directory_path=$1
+
+if [ -z "$directory_path" ]; then
+  echo "Please provide the directory path."
+  exit 1
+fi
+
+cd "$directory_path"
+
+for file in *; do
+  if [ -x "$file" ]; then
+    "./$file"
   fi
 done
-
+cd -
